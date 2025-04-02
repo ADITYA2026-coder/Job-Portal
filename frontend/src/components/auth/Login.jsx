f24606ba647c30965be674f0ec242b8d28db6311
ADITYA2026-coder <adityatripathi16804@gmail.com>
2025-04-02 17:38:17 +0530

    HeroSection

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -101,7 +101,7 @@ const Login = () => {
 					{loading ? (
 						<Button className="w-full my-4">
 							<Loader2 className="mr-2 h-4 w-4 animate-spin" />
-							Please wait{' '}
+							Please wait
 						</Button>
 					) : (
 						<Button type="submit" className="w-full my-4">
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,6 +8,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { USER_API_END_POINT } from '@/utils/constant';
 import { toast } from 'sonner';
+import { useDispatch } from 'react-redux';
+import { setLoading } from '@/redux/authSlice';
 
 const Signup = () => {
 	const [input, setInput] = useState({
@@ -18,6 +20,8 @@ const Signup = () => {
 		role: '',
 		file: '',
 	});
+	const { loading } = useSelector((store) => store.auth);
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const changeEventHandler = (e) => {
 		setInput({ ...input, [e.target.name]: e.target.value });
@@ -37,7 +41,7 @@ const Signup = () => {
 			formData.append('file', input.file);
 		}
 		try {
-			console.log(formData);
+			dispatch(setLoading(true));
 			const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
 				headers: { 'Content-Type': 'multipart/form-data' },
 				withCredentials: true,
@@ -56,6 +60,8 @@ const Signup = () => {
 			} else {
 				toast.error('Request failed. Check network or API.');
 			}
+		} finally {
+			dispatch(setLoading(false));
 		}
 	};
 	return (
@@ -142,9 +148,16 @@ const Signup = () => {
 							/>
 						</div>
 					</div>
-					<Button type="submit" className="w-full my-4">
-						Signup
-					</Button>
+					{loading ? (
+						<Button className="w-full my-4">
+							<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+							Please wait
+						</Button>
+					) : (
+						<Button type="submit" className="w-full my-4">
+							Signup
+						</Button>
+					)}
 					<span className="text-sm">
 						Already have an account?
 						<Link to="/login" className="text-blue-600">
